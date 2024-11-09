1e4ab6c18caccd4ac63b70e2beb7744bdbaa71ce
Lucperrom <luciaperezromero07@gmail.com>
2024-11-09 20:54:43 +0100

    config: Configuración SpringBoot, Maven, MongoDB#1

---
+++ microservicio-valoraciones/src/test/java/teachme5/microservicio_valoraciones/MicroservicioValoracionesApplicationTests.java
@@ -0,0 +1,13 @@
+package teachme5.microservicio_valoraciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioValoracionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
