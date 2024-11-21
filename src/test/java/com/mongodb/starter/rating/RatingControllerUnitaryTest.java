c3e4a19c382b09eef99520ed9fe6f9ec5514aea3
Lucperrom <luciaperezromero07@gmail.com>
2024-11-21 14:30:43 +0100

    fix: correcciones #6

--- src/test/java/com/mongodb/starter/rating/RatingControllerUnitaryTest.java
+++ src/test/java/com/mongodb/starter/rating/RatingControllerUnitaryTest.java
@@ -19,12 +19,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.starter.util.MessageResponse;
