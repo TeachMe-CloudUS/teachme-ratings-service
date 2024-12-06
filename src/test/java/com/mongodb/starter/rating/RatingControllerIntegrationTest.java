919c219b0e65ae3730a5ffea461aea609f581644
Lucperrom <luciaperezromero07@gmail.com>
2024-12-06 19:25:37 +0100

    conf: Configuraciones necesarias para despliegue en Kubernetes #9

--- src/test/java/com/mongodb/starter/rating/RatingControllerIntegrationTest.java
+++ src/test/java/com/mongodb/starter/rating/RatingControllerIntegrationTest.java
@@ -9,6 +9,8 @@ import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootTest()
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+
 class RatingControllerIntegrationTest {
 
     @Autowired
