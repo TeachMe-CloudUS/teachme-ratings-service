bae3cdb2b68f0f73e1e9155348916bb7b3ab89d9
danmoltor2 <danmoltor.us@gmail.com>
2024-12-19 11:08:14 +0100

    conf: Integración de circuit breaker #32

---
+++ src/main/java/com/mongodb/starter/student/StudentDto.java
@@ -0,0 +1,17 @@
+package com.mongodb.starter.student;
+
+import lombok.Data;
+
+@Data
+public class StudentDto {
+    private String id; 
+    private String userName; 
+
+    public StudentDto() {}
+
+    public StudentDto(String id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+}
